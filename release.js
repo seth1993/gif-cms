8e6ddc7fc5bf9523709d410fc7899ac7560c04d9
Seth Bailey <seth.bailey@byu.edu>
2016-12-22 11:07:27 -0700

    Made changes to branding images

--- release.js
+++ release.js
@@ -9,7 +9,7 @@ var options = {};
 
 // options.ip = '127.0.0.1';
 // options.port = parseInt(process.argv[2]);
-// options.config = { name: 'total.js' };
+options.config = { name: 'PROVO SPOT' };
 // options.https = { key: fs.readFileSync('keys/agent2-key.pem'), cert: fs.readFileSync('keys/agent2-cert.pem')};
 
 /**
